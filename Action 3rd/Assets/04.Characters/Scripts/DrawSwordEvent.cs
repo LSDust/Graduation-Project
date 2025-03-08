880e16c7a3f57dd2d9a4f6863c9fa3ad56127869
Quarter Night <2250919876@qq.com>
2025-03-09 05:45:14 +0800

    1.添加了Cinemachine相机控制

--- Action 3rd/Assets/04.Characters/Scripts/DrawSwordEvent.cs
+++ Action 3rd/Assets/04.Characters/Scripts/DrawSwordEvent.cs
@@ -1,25 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Action3rd
 {
     public class DrawSwordEvent : MonoBehaviour
     {
-        /// <summary>
-        /// 剑鞘
-        /// </summary>
-        [SerializeField] private Transform scabbard;
         [SerializeField] private Transform swordBody;
+
         /// <summary>
-        /// 持剑手
+        ///     持剑手
         /// </summary>
         [SerializeField] private Transform swordHead;
-        
+
         private void DrawSword()
         {
-            this.swordBody.SetParent(this.swordHead,true);
+            swordBody.SetParent(swordHead, true);
         }
     }
 }
