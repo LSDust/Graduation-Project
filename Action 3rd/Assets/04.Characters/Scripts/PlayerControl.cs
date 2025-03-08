8ccc7285872e3f3779e8516c642e6b972cdf8a17
Quarter Night <2250919876@qq.com>
2025-03-09 05:26:46 +0800

    1.调整动画

--- Action 3rd/Assets/04.Characters/Scripts/PlayerControl.cs
+++ Action 3rd/Assets/04.Characters/Scripts/PlayerControl.cs
@@ -2,27 +2,22 @@ using UnityEngine;
 
 namespace Action3rd
 {
-    public class PlayerControl : MonoBehaviour
+    public class PlayerControl : Singleton<PlayerControl>
     {
         private Animator _animator;
 
-        private void Awake()
+        protected override void Awake()
         {
+            base.Awake();
             _animator = GetComponent<Animator>();
         }
 
-        // Start is called before the first frame update
-        private void Start()
-        {
-        }
-
-        // Update is called once per frame
         private void Update()
         {
             Vector3 dir =
-                ((Input.GetAxis("Horizontal") * Vector3.right) + (Input.GetAxis("Vertical") * Vector3.forward))
-                .normalized;
+                (Input.GetAxis("Horizontal") * Vector3.right) + (Input.GetAxis("Vertical") * Vector3.forward);
             _animator.SetFloat("Speed", dir.magnitude * 20f);
+
             if (Input.GetMouseButtonDown(0))
             {
                 _animator.SetTrigger("Attack");
---
+++ Action 3rd/Assets/05.Common/Scripts/Singleton.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Action3rd
+{
+    /// <summary>
+    ///     单例组件父类
+    /// </summary>
+    /// <typeparam name="T">单例子类</typeparam>
+    public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        /// <summary>
+        ///     唯一实例
+        /// </summary>
+        public static T Instance { get; protected set; }
+
+        /// <summary>
+        ///     检查唯一实例是否存在
+        /// </summary>
+        public static bool InstanceExists => Instance != null;
+
+        /// <summary>
+        ///     Awake(构造)函数将单例与实例关联
+        /// </summary>
+        protected virtual void Awake()
+        {
+            if (InstanceExists)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = (T)this;
+            }
+        }
+
+        /// <summary>
+        ///     将唯一实例清除
+        /// </summary>
+        protected virtual void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                Instance = null;
+            }
+        }
+    }
+}
