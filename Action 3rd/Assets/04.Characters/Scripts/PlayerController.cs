b9304a01a48e741ec0e3bfe692dd97b80d030bd4
Quarter Night <2250919876@qq.com>
2025-03-09 23:15:48 +0800

    1.第三人称控制场景原型搭建

--- Action 3rd/Assets/04.Characters/Scripts/PlayerController.cs
+++ Action 3rd/Assets/04.Characters/Scripts/PlayerController.cs
@@ -1,29 +1,54 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Action3rd
 {
     public class PlayerController : Singleton<PlayerController>
     {
         private static readonly int Attack = Animator.StringToHash("Attack");
-
-        // private static readonly int Speed = Animator.StringToHash("Speed");
+        private static readonly int Speed = Animator.StringToHash("Speed");
         private Animator _animator;
+
+        /// <summary>
+        ///     移动方向
+        /// </summary>
+        private Vector2 _playerInputVec;
+
         private Rigidbody _rigidbody;
+
 
         protected override void Awake()
         {
             base.Awake();
             _animator = GetComponent<Animator>();
+            _rigidbody = GetComponent<Rigidbody>();
         }
 
         private void Update()
         {
-            // Vector3 direction = (Input.GetAxis("Horizontal") * Vector3.right) +
-            //                     (Input.GetAxis("Vertical") * Vector3.forward);
-            // transform.rotation = Quaternion.LookRotation(direction);
-            // _animator.SetFloat(Speed, direction.magnitude);
+            if (_playerInputVec != Vector2.zero)
+            {
+                //todo:拔剑和攻击时不允许旋转
+                transform.rotation = Quaternion.LookRotation(new Vector3(_playerInputVec.x, 0, _playerInputVec.y));
+            }
+        }
 
-            if (Input.GetMouseButtonDown(0))
+        public void GetPlayerMoveInput(InputAction.CallbackContext ctx)
+        {
+            _playerInputVec = ctx.ReadValue<Vector2>();
+
+            //todo:不会渐变
+            _animator.SetFloat(Speed, _playerInputVec.magnitude);
+        }
+
+        public void GetPlayerLookInput(InputAction.CallbackContext ctx)
+        {
+            // Debug.Log(ctx.phase.ToString());
+        }
+
+        public void GetPlayerAttackInput(InputAction.CallbackContext ctx)
+        {
+            if (ctx.performed)
             {
                 _animator.SetTrigger(Attack);
             }
