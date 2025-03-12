ec70efd730cb89fa65af9c3fba28f959744a9680
Quarter Night <2250919876@qq.com>
2025-03-12 21:05:27 +0800

    1.设置界面雏形

--- Action 3rd/Assets/02.UI/Scripts/Panel/BagPanel/BagPanel.cs
+++ Action 3rd/Assets/02.UI/Scripts/Panel/BagPanel/BagPanel.cs
@@ -1,10 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace Action3rd.UI
 {
     public class BagPanel : BasePanel
     {
+        [SerializeField] private Button exitButton;
+
         private void Awake()
         {
-            // settingButton.onClick.AddListener(OpenSettingPanel);
+            exitButton.onClick.AddListener(CloseBagPanel);
+        }
+
+        private void CloseBagPanel()
+        {
+            PanelManager.ClosePanel();
         }
 
         private void Start()
