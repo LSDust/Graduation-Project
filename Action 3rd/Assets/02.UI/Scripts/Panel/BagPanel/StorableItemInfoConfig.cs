9b988fab9299ca0a36ded8d446f0c5e137750a07
Quarter Night <2250919876@qq.com>
2025-03-23 00:54:37 +0800

    1.可收集物品的数据重构

--- Action 3rd/Assets/02.UI/Scripts/Panel/BagPanel/StorableItemInfoConfig.cs
+++ Action 3rd/Assets/02.UI/Scripts/Panel/BagPanel/StorableItemInfoConfig.cs
@@ -4,54 +4,15 @@ using UnityEngine;
 
 namespace Action3rd
 {
-    public class StorableItemData
-    {
-        public int ItemInfoIndex;
-
-        //动态数据
-        public string ItmId;
-        public bool IsNew;
-        public int Quantity;
-        public int WeaponLevel;
-
-        public StorableItemData(int itemInfoIndex, string itmId, int level = 0, bool isNew = true)
-        {
-            ItemInfoIndex = itemInfoIndex;
-            ItmId = itmId;
-            WeaponLevel = level;
-            IsNew = isNew;
-        }
-    }
-
     [CreateAssetMenu(fileName = "StorableItemInfoConfig.asset", menuName = "Action3rd/StorableItemInfoConfig")]
     public class StorableItemInfoConfig : ScriptableObject
     {
         public List<StorableItemInfo> items = new List<StorableItemInfo>();
-
-        private void OnValidate()
-        {
-//             foreach (StorableItemInfo ii in items)
-//             {
-//                 if (ii.itemIcon != null)
-//                 {
-//                     ii.fileName = ii.itemIcon.name;
-// // #if UNITY_EDITOR
-//                     // ii.atlasPath = UnityEditor.AssetDatabase.GetAssetPath(ii.itemIcon);
-// // #endif
-//                 }
-//                 else
-//                 {
-//                     ii.fileName = string.Empty;
-//                     // ii.atlasPath = string.Empty;
-//                 }
-//             }
-        }
     }
 
     [Serializable]
     public class StorableItemInfo
     {
-        // [HideInInspector] public string fileName;
         public Sprite itemIcon;
         public string itemName;
         public StorableItemType storableItemType;
@@ -59,4 +20,22 @@ namespace Action3rd
     }
 
     public enum StorableItemType { 武器, 食物 }
+
+    public class StorableItemData
+    {
+        public readonly int ItemInfoIndex;
+
+        //动态数据
+        public readonly string ItmId;
+        public int Durability;
+        public int Quantity;
+
+        public StorableItemData(int itemInfoIndex, string itmId, int durability = 100, int quantity = 1)
+        {
+            ItemInfoIndex = itemInfoIndex;
+            ItmId = itmId;
+            Durability = durability;
+            Quantity = quantity;
+        }
+    }
 }
