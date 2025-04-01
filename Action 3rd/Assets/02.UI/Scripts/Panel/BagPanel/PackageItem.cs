b3c3896a02b59f8fc4a865ed240af8b38b3859eb
Quarter Night <2250919876@qq.com>
2025-04-01 14:26:43 +0800

    1.背包加了悬停物品时有选中框

--- Action 3rd/Assets/02.UI/Scripts/Panel/BagPanel/PackageItem.cs
+++ Action 3rd/Assets/02.UI/Scripts/Panel/BagPanel/PackageItem.cs
@@ -7,16 +7,29 @@ using UnityEngine.UI;
 
 namespace Action3rd.UI
 {
-    public class PackageItem : MonoBehaviour, IPointerClickHandler
+    public class PackageItem : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
     {
-        public event Action<PackageItem> OnClick;
-        public Image iconImage;
-        public StorableItemData StorableItemData;
+        [Header("子物体")] public Image iconImage;
+
+        public GameObject selectedEdge;
         [Tooltip("文本")] public TMP_Text itemText;
+
+        [Header("数据")] public StorableItemData StorableItemData;
+        public event Action<PackageItem> OnClick;
 
         public void OnPointerClick(PointerEventData eventData)
         {
             this.OnClick?.Invoke(this);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            this.selectedEdge.SetActive(true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            this.selectedEdge.SetActive(false);
         }
     }
 }
